1e8af0898ae193d14210d70fc2d074e109f54587
kahu08 <peterkahurani@gmail.com>
2017-04-12 13:01:00 +0300

    created an index display for expenses and made the expense partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,3 @@
 Rails.application.routes.draw do
-  get 'debts/index'
-
-  get 'debts/show'
-
-  get 'debts/edit'
-
-  get 'debts/new'
-
-  get 'loans/index'
-
-  get 'loans/show'
-
-  get 'loans/edit'
-
-  get 'loans/new'
-
-  get 'expenses/index'
-
-  get 'expenses/show'
-
-  get 'expenses/edit'
-
-  get 'expenses/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: "expense#index"
 end
