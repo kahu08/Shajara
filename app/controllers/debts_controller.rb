ef34ee0ece29a9a2db504b9f5b762a1d75ce6809
kahu08 <peterkahurani@gmail.com>
2017-04-13 12:41:20 +0300

    update function on debts controller fixed

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -22,6 +22,16 @@ class DebtsController < ApplicationController
   end
 
   def edit
+    @debt = Debt.find(params[:id])
+  end
+
+  def  update
+    @debt = Debt.find(params[:id])
+    if @debt.update(debt_params)
+      redirect_to debts_path
+    else
+      render :edit
+    end
   end
 
   def new
